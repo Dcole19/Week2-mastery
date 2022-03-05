4139ec382a370d93106ac1046928baa5dfc6d135
Dale Cole <dale.cole2@yahoo.com>
2022-03-05 09:42:46 -0700

    Mastery Progress week 2

---
+++ masteryProgress.java
@@ -0,0 +1,160 @@
+import java.util.Scanner;
+
+public class masteryProgress {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+    
+    
+    
+ //   int userNumber = 40; 
+    
+ //   if (userNumber > 100 || userNumber < 1) {
+ //       System.out.println("Your number was not between 1 and 100.");
+  //    } else {
+  //        while (userNumber >= 1) {
+ //           System.out.println(userNumber);
+  //          userNumber++;
+   //       } 
+// System.out.println("---------------------------");    
+  //  int counter = userNumber;
+    
+  //        if ( userNumber <= 100 && userNumber >= 1 ) {
+  //          while ( counter <= 100 ) {
+   //             System.out.println(counter++);
+    //        }
+   //     } else {
+   //         System.out.println("Your number was not between 1 and 100.");
+   //     } 
+    
+// System.out.println("---------------------------"); 
+    
+// Scanner in = new Scanner(System.in);  
+// String userWord = in.nextLine();
+// int wordLength = userWord.length();
+// int counter = wordLength;
+//    do {
+ //     System.out.println(userWord);
+ //     if ( wordLength < 5 ) {
+ //         break;
+ //     } 
+ //     counter--;
+//  } while ( counter > 0 );
+    
+    
+// System.out.println("---------------------------");  
+//    int num = 4;
+  
+//    for (int i = 1; i < 11; i++) {
+//      System.out.println(num + " x " + i + " " + "=" + " " + (num * i));
+//      }
+    
+// System.out.println("---------------------------");  
+    
+    
+
+ /* int numToAdd = 5;
+ int howManyTimes = 5;
+ 
+ do {
+   numToAdd += numToAdd;
+   howManyTimes--;
+} while (howManyTimes > 0);
+
+System.out.println(numToAdd);
+*/    
+    
+// System.out.println("---------------------------");      
+
+
+
+   
+    
+//   String userWord = "hello";
+//   String longString ="";
+
+
+//   for ( int i = 0; i < userWord.length(); i++ ) {
+//     longString += userWord;
+//   }
+
+
+//  System.out.println(longString);
+    
+//   int counter = 0; 
+//  for (int i = 1; i < 100; i += 2) {
+//    counter++;
+// }
+//    System.out.println(counter);
+
+    
+ //   System.out.println("---------------------------");
+    
+// Scanner in = new Scanner(System.in);
+
+// String randomWord = in.next();
+
+// if (randomWord.length() >= 10) {
+//  System.out.println("That is a really long word!");
+// }else {
+//  System.out.println("Your word is not that long.");
+// }
+  
+    
+ //   System.out.println("---------------------------");
+    
+// int num1 = 156;
+// Scanner in = new Scanner(System.in);
+ 
+// int myNumber = in.nextInt();
+ 
+// if (myNumber > num1) {
+//   System.out.println(num1 + " is smaller than my number," + myNumber);
+//   }else if (myNumber < num1) {
+//     System.out.println(num1 + " is larger than my number," +myNumber);
+//   }else {
+//     System.out.println(num1 + " is equal to my number," + myNumber);
+//   }
+    
+ 
+ //   System.out.println("---------------------------");  
+    
+//   Scanner in = new Scanner(System.in);
+//   int numericGrade = in.nextInt();
+   
+//   if(numericGrade >= 90) {
+//     System.out.println("A");
+//   }else if (numericGrade >= 80) {
+//     System.out.println("B");
+//   }else if (numericGrade >= 70) {
+//     System.out.println("C");
+//   }else if (numericGrade >= 60) {
+//     System.out.println("D");
+//   }else {
+//     System.out.println("F");
+//   }
+    
+    
+ //   System.out.println("---------------------------"); 
+    
+   Scanner in = new Scanner(System.in);
+   int num1 = in.nextInt();
+   int num2 = in.nextInt();
+   int num3 = in.nextInt();
+   int num4 = in.nextInt();
+   
+   
+   
+  if (num1 >= num2) {
+    if (num3 >= num4) {
+      System.out.println("both statements are true");
+    }
+  }
+  System.out.println("program ended");
+   
+   
+   
+   
+    
+}}
